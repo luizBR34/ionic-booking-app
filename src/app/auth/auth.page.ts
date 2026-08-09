3dc79e5170a4a060d82a91b370b556b44b67331e
Luiz Fernando Povoa <luizfer1@yahoo.com.br>
2026-08-08 23:08:31 -0300

    New Offers page

--- src/app/auth/auth.page.ts
+++ src/app/auth/auth.page.ts
@@ -9,16 +9,15 @@ import { NgForm } from '@angular/forms';
   templateUrl: './auth.page.html',
   styleUrls: ['./auth.page.scss'],
 })
-export class AuthPage implements OnInit {
+export class AuthPage {
   isLoading = false;
+  isLogin = true;
 
   constructor(
     private authService: AuthService,
     private router: Router,
     private loadingCtrl: LoadingController,
   ) {}
-
-  ngOnInit() {}
 
   onLogin() {
     this.isLoading = true;
@@ -39,6 +38,23 @@ export class AuthPage implements OnInit {
   }
 
   onSubmit(form: NgForm) {
-    console.log(form);
+    if (!form.valid) {
+      return;
+    }
+    const email = form.value.email;
+    const password = form.value.password;
+
+    console.log(email, password);
+
+    //SEND A REQUEST TO THE SERVER
+    if (this.isLogin) {
+      this.onLogin();
+    } else {
+      //SIGN UP
+    }
+  }
+
+  onSwitchAuthMode() {
+    this.isLoading = !this.isLoading;
   }
 }
